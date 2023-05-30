859d6e02649ecc7932242904e95126b7e4a85c31
rslgomes <rslgomes@gmail.com>
2023-05-30 16:25:35 -0300

    Added work done so far from computer

---
+++ js/script.js
@@ -0,0 +1,33 @@
+fetch('products.json')
+  .then((response) => response.json())
+  .then((data) => {
+    const products = data.products;
+
+    let productListHTML = '';
+
+    products.forEach(product => {
+      productListHTML += `
+        <div>
+          <span>${product.name} - $${product.price}</span>
+          <button class="add-to-cart" data-product-id="${product.id}">Add to Cart</button>
+        </div>
+      `;
+    });
+
+    const productListContainer = document.getElementById('product-list');
+    productListContainer.innerHTML = productListHTML;
+
+    const addToCartButtons = document.querySelectorAll('.add-to-cart');
+    addToCartButtons.forEach(button => {
+      button.addEventListener('click', addToCart);
+    });
+
+    function addToCart(event) {
+      const productId = event.target.dataset.productId;
+      // Handle product to cart
+      console.log('Product added to cart:', productId);
+    }
+  })
+  .catch((error) => {
+    console.error('Error:', error);
+  });
